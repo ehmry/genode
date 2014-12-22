ac7a9cec691c7336bd5004d13c1f18ff0e8cf63d
Emery Hemingway <emery@vfemail.net>
2014-12-22 14:56:57 -0500

    nix: fix transform-binary for i686

--- tool/build/transform-binary.sh
+++ tool/build/transform-binary.sh
@@ -3,4 +3,4 @@ source $genodeEnv/setup
 MSG_CONVERT $symbolName
 
 echo ".global ${symbolName}_start, ${symbolName}_end; .data; .align 4; ${symbolName}_start:; .incbin \"$binary\"; ${symbolName}_end:" | \
-    $as $asOpt -f -o $out -
+    $as $asFlags -f -o $out -
