96c7aac75cce5abd1ab1932d053ffae241bb0dc6
Emery Hemingway <emery@vfemail.net>
2015-01-24 11:20:00 -0500

    nix: not executing runs with dynamic linking on linux

--- repos/base-linux/tool/run/default.nix
+++ repos/base-linux/tool/run/default.nix
@@ -27,6 +27,8 @@ let
       }
     ];
 
+  dynamicLinking = tool.anyShared (map (x: x.source) contents');
+
   imageDir = bootImage { inherit name; contents = contents'; };
 in
 derivation {
@@ -50,15 +52,29 @@ derivation {
       ./linux.exp
     ];
 
-  inherit automatic imageDir testScript;
+  inherit imageDir testScript;
+
+  automatic =
+    if dynamicLinking
+    then builtins.trace "${name}-run contains shared libraries, not executing." false
+    else automatic;
 
   userScript =
     ''
       #!${nixpkgs.expect}/bin/expect
       set image_dir ${imageDir}
+      set name ${name}
       source ${ ../../../../tool/run}
       source ${./linux.exp}
 
+      set temp_dir /tmp/$name-run-[pid]
+      file mkdir $temp_dir
+      foreach f [glob $image_dir/*] {
+      	file link $temp_dir/[file tail $f] $f
+      }
+
+      cd $temp_dir
       ${testScript}
+      file delete $temp_dir
     '';
 }
