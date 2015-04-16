1988ba87a039fe8a75dd0fa04675a834c8cae724
Emery Hemingway <emery@vfemail.net>
2015-04-16 17:59:12 -0400

    changes to nichts_client

--- repos/ports/src/test/nichts_client/main.cc
+++ repos/ports/src/test/nichts_client/main.cc
@@ -6,21 +6,23 @@
 
 
 /* Genode includes */
-#include <base/printf.h>
 #include <nichts_store_session/connection.h>
+#include <base/allocator_avl.h>
+#include <base/printf.h>
 #include <os/config.h>
 
 
 int main(int, char **)
 {
-	Nichts_store::Connection srv;
+	Genode::Allocator_avl    tx_alloc(Genode::env()->heap());
+	Nichts_store::Connection store();
 
 	Genode::config()->xml_node().for_each_sub_node("drv", [&] (Genode::Xml_node drv_node) {
 		char path[Nichts_store::MAX_PATH_LEN];
 		drv_node.value(path, sizeof(path));
 
 		try {
-			srv.realise(path);
+			store.realise(path);
 		}
 		catch (Nichts_store::Invalid_derivation) {
 			PERR("%s was invalid", path);
