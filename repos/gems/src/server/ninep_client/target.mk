c06d408dc5b97251b8a978db87574af0f49e0729
Emery Hemingway <emery@vfemail.net>
2015-04-28 17:57:31 -0400

    nix: remove old ninep_client

--- repos/gems/src/server/ninep_client/target.mk
+++
@@ -1,3 +0,0 @@
-TARGET = 9p_client
-SRC_CC = main.cc
-LIBS   = base config server libc libc_lwip_nic_dhcp
--- repos/gems/src/server/ninep_fs/default.nix
+++ repos/gems/src/server/ninep_fs/default.nix
@@ -1,7 +1,7 @@
 { linkComponent, compileCC, server, config, libc, libc_lwip_nic_dhcp }:
 
 linkComponent rec {
-  name = "9p_client";
+  name = "9p_fs";
   libs = [ server config libc libc_lwip_nic_dhcp ];
   objects = compileCC { src = ./main.cc; inherit libs; };
   runtime.provides = [ "File_system" ];
