0a801cd0cc34978a75205236f2bf815935d4ab1e
Emery Hemingway <emery@vfemail.net>
2015-04-28 17:08:26 -0400

    nix: fix libs.x86emu

--- repos/libports/lib/mk/x86emu.nix
+++ repos/libports/lib/mk/x86emu.nix
@@ -2,18 +2,20 @@
 # x86 real-mode emulation library
 #
 
-{ linkStaticLibrary, compileCRepo, addPrefix, x86emuSrc }:
+{ linkStaticLibrary, compileCRepo, fromDir, x86emuSrc }:
 
 let
   externalIncludes =
     [  ../../include/x86emu x86emuSrc.include "${x86emuSrc.include}/x86emu" ];
+  headers = [ ../../include/x86emu/sys/types.h ];
 in
 linkStaticLibrary {
   name = "x86emu";
 
   externalObjects = compileCRepo {
+    inherit headers;
     extraFlags = [ "-fomit-frame-pointer" "-I." ];
-    sources = addPrefix "${x86emuSrc}/"
+    sources = fromDir x86emuSrc
       [ "decode.c" "fpu.c" "ops.c" "ops2.c" "prim_ops.c" "sys.c" ];
     externalIncludes = [ x86emuSrc ] ++ externalIncludes;
   };
