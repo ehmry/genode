04e13fda2c6ccf4ec32ef0c9fc8fe38f87639815
Emery Hemingway <emery@vfemail.net>
2017-08-11 08:03:43 -0500

    fs_replay update

--- repos/os/src/app/fs_replay/main.cc
+++ repos/os/src/app/fs_replay/main.cc
@@ -14,14 +14,40 @@
 #include <base/debug.h>
 
 #include <file_system_session/connection.h>
+#include <log_session/connection.h>
 #include <base/attached_rom_dataspace.h>
 #include <base/heap.h>
 #include <base/component.h>
+#include <util/print_lines.h>
 
 
 namespace Fs_replay {
 	using namespace Genode;
 	using namespace File_system;
+
+	template <typename HANDLE_TYPE>
+	struct Open_handle
+	{
+		HANDLE_TYPE const handle;
+		Id_space<Open_handle<HANDLE_TYPE>>::Element space_elem;
+
+		Open_handle(HANDLE_TYPE &h, Id_space<Open_handle<HANDLE_TYPE>> &space)
+		: handle(h), space_elem(*this, h.id, space) { }
+	};
+
+	/*
+	struct Open_file
+	{
+	};
+
+	struct Open_symlink
+	{
+	};
+
+	typedef Genode::Id_space<Open_dir>     Directory_space;
+	typedef Genode::Id_space<Open_file>    File_space;
+	typedef Genode::Id_space<Open_symlink> Symlink_space;
+	*/
 
 	struct Main;
 
@@ -29,7 +55,7 @@ namespace Fs_replay {
 	{
 		try { return config.sub_node("trace"); }
 		catch (Xml_node::Nonexistent_sub_node) {
-			return Xml_node("trace"); }
+			return Xml_node("<trace/>"); }
 	}
 }
 
@@ -46,6 +72,12 @@ struct Fs_replay::Main
 
 	File_system::Connection fs;
 
+/*
+	Directory_space dir_space;
+	File_space      file_space;
+	Symlink_space   symlink_space;
+ */
+
 	Main(Genode::Env &env, Xml_node config)
 	:
 		env(env),
@@ -60,6 +92,29 @@ struct Fs_replay::Main
 
 void Fs_replay::Main::replay()
 {
+	auto line_func = [&] (char const *line)
+	{
+		if (MATCH_VERB("cl", line)) { } else
+		if (MATCH_VERB("dh", line)) { } else
+		if (MATCH_VERB("do", line)) { } else
+		if (MATCH_VERB("fh", line)) { } else
+		if (MATCH_VERB("fo", line)) { } else
+		if (MATCH_VERB("nh", line)) { } else
+		if (MATCH_VERB("no", line)) { } else
+		if (MATCH_VERB("re", line)) { } else
+		if (MATCH_VERB("rn", line)) { } else
+		if (MATCH_VERB("sh", line)) { } else
+		if (MATCH_VERB("sl", line)) { } else
+		if (MATCH_VERB("st", line)) { } else
+		if (MATCH_VERB("sy", line)) { } else
+		if (MATCH_VERB("tr", line)) { } else
+		if (MATCH_VERB("us", line)) { } else
+		if (MATCH_VERB("wr", line)) { } else
+		{ }
+};
+
+	print_lines<Log_session::MAX_STRING_LEN>(
+		trace_rom.local_addr<char const>(), trace_rom.size(), line_func);
 }
 
 
