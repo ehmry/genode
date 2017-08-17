36e5a28bfb020f2246797bd736b06021d437abd3
Emery Hemingway <emery@vfemail.net>
2017-08-17 11:52:26 -0500

    Nim toolchain improvements
    - Fix fatal exception handling so that stack traces are dumped
    - Add 'include/nim' directories to Nim module search path
    - Enable release optimizations for release builds
    
    Fix #2493

--- repos/base/mk/generic.mk
+++ repos/base/mk/generic.mk
@@ -95,8 +95,10 @@ $(warning NIM_CPU not defined for any of the following SPECS: $(SPECS))
 else
 
 NIM_MAKEFILES := $(foreach X,$(SRC_NIM),$(X).mk)
-NIM_ARGS  = --compileOnly --os:genode --cpu:$(NIM_CPU)
-NIM_ARGS += --verbosity:0 --hint[Processing]:off --nimcache:.
+NIM_ARGS  = \
+	--compileOnly --os:genode --cpu:$(NIM_CPU) --noCppExceptions \
+	--verbosity:0 --hint[Processing]:off --nimcache:.
+NIM_ARGS += $(foreach X,$(call select_from_repositories,include/nim),--path:$(X))
 NIM_ARGS += $(NIM_OPT)
 
 # Generate the C++ sources and compilation info
