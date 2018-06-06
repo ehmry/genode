6f93d7a662a4f1ca7696d76c513f1d2d87c49d26
Norman Feske <norman.feske@genode-labs.com>
2018-06-06 12:29:51 +0200

    sculpt: track both manual and managed NIC target
    By tracking the states for an interactive selected NIC target (managed)
    and a manual-defined NIC target (config/nic_router) separately, the
    sculpt manager becames able to present the user with the ability to
    interactively disable and re-enable a manually-managed network
    configuration.

--- repos/gems/src/app/sculpt_manager/model/nic_target.h
+++ repos/gems/src/app/sculpt_manager/model/nic_target.h
@@ -21,20 +21,45 @@ namespace Sculpt { struct Nic_target; }
 
 struct Sculpt::Nic_target : Noncopyable
 {
-	enum Type { OFF, LOCAL, WIRED, WIFI } type { OFF };
-
 	enum Policy { MANAGED, MANUAL } policy { MANAGED };
 
 	bool manual()  const { return policy == MANUAL; }
 	bool managed() const { return policy == MANAGED; }
 
-	bool local()   const { return type == LOCAL; }
-	bool wired()   const { return type == WIRED; }
-	bool wifi()    const { return type == WIFI; }
+	enum Type { OFF, LOCAL, WIRED, WIFI };
 
-	bool nic_router_needed() const { return type != OFF; }
+	/**
+	 * Interactive selection by the user, used when managed policy is in effect
+	 */
+	Type managed_type { OFF };
 
-	bool ready() const { return type == WIRED || type == WIFI; }
+	/**
+	 * Selection by the manually-provided NIC-router configuration
+	 */
+	Type manual_type { OFF };
+
+	/**
+	 * Return currently active NIC target type
+	 */
+	Type type() const
+	{
+		/*
+		 * Enforce the user's interactive choice to disable networking
+		 * even if the NIC target is manually managed.
+		 */
+		if (managed_type == OFF)
+			return OFF;
+
+		return manual() ? manual_type : managed_type;
+	}
+
+	bool local() const { return type() == LOCAL; }
+	bool wired() const { return type() == WIRED; }
+	bool wifi()  const { return type() == WIFI; }
+
+	bool nic_router_needed() const { return type() != OFF; }
+
+	bool ready() const { return type() == WIRED || type() == WIFI; }
 };
 
 #endif /* _MODEL__NIC_TARGET_H_ */
