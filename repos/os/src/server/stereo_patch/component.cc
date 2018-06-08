651445a727687fc0f2f71a8ce5380ebfc2f85b13
Emery Hemingway <ehmry@posteo.net>
2018-06-08 21:08:33 +0200

    Stereo patch fixup

--- repos/os/src/server/stereo_patch/component.cc
+++ repos/os/src/server/stereo_patch/component.cc
@@ -44,10 +44,9 @@ struct Patch::State
 {
 	Audio::Stream_buffer buffer;
 
-	Genode::Signal_context_capability      overrun_cap { };
-	Genode::Signal_context_capability     underrun_cap { };
-	Genode::Signal_context_capability source_reset_cap { };
-	Genode::Signal_context_capability   sink_reset_cap { };
+	Genode::Signal_context_capability    start_cap { };
+	Genode::Signal_context_capability    reset_cap { };
+	Genode::Signal_context_capability underrun_cap { };
 
 	State(Genode::Env &env) : buffer(env.pd(), env.rm()) { }
 };
@@ -59,44 +58,27 @@ class Audio_in::Session_component :
 	private:
 
 		Patch::State       &_common_state;
-		Genode::Entrypoint &_ep;
-
-		bool _stopped = true; /* state */
 
 	public:
 
-		Parent::Server::Id const id;
+		Session_component(Patch::State &state)
+		: _common_state(state) { }
 
-		Session_component(Patch::State &state, Entrypoint &ep, Id id)
-		: _common_state(state), _ep(ep), id(id)
-		{
-		}
+		/************************
+		 ** Audio_in interface **
+		 ************************/
 
-		~Session_component()
-		{
-			_ep.dissolve(*this);
-		}
-
-
-		/**************
-		 ** Signals  **
-		 **************/
-
-		Genode::Signal_context_capability underrun_sigh() override { PDBG();
-			return _common_state.underrun_cap; }
-
-		void reset_sigh(Genode::Signal_context_capability sigh) override { PDBG();
-			_common_state.sink_reset_cap = sigh; }
-
-		/***********************
-		 ** Session interface **
-		 ***********************/
-
-		void start() override { PDBG(); _stopped = false; }
-		void stop()  override { PDBG(); _stopped = true; }
-
-		Genode::Dataspace_capability dataspace() { PDBG();
+		Genode::Dataspace_capability dataspace() {
 			return _common_state.buffer.cap(); }
+
+		void start_sigh(Signal_context_capability sigh) override {
+			_common_state.start_cap = sigh; }
+
+		void reset_sigh(Signal_context_capability sigh) override {
+			_common_state.reset_cap = sigh; }
+
+		Signal_context_capability underrun_sigh() override {
+			return _common_state.underrun_cap; }
 };
 
 
@@ -106,45 +88,32 @@ class Audio_out::Session_component :
 	private:
 
 		Patch::State       &_common_state;
-		Genode::Entrypoint &_ep;
-
-		bool _stopped = true; /* state */
 
 	public:
 
-		Id const id;
+		Session_component(Patch::State &state)
+		: _common_state(state) { }
 
-		Session_component(Patch::State &state, Entrypoint &ep, Id id)
-		: _common_state(state), _ep(ep), id(id) { }
 
-		~Session_component()
+		/************************
+		 ** Audio_in interface **
+		 ************************/
+
+		Genode::Dataspace_capability dataspace() {
+			return _common_state.buffer.cap(); }
+
+		void start() override
 		{
-			_ep.dissolve(*this);
+			if (_common_state.start_cap.valid())
+				Signal_transmitter(_common_state.start_cap).submit();
 		}
-
-		/**************
-		 ** Signals  **
-		 **************/
 
 		void underrun_sigh(Signal_context_capability sigh) override
-		{ PDBG();
+		{
 			_common_state.underrun_cap = sigh;
-			if (_common_state.sink_reset_cap.valid())
-				Signal_transmitter(_common_state.sink_reset_cap).submit();
+			if (_common_state.reset_cap.valid())
+				Signal_transmitter(_common_state.reset_cap).submit();
 		}
-
-		void reset_sigh(Signal_context_capability sigh) override { PDBG();
-			_common_state.source_reset_cap = sigh; }
-
-		/***********************
-		 ** Session interface **
-		 ***********************/
-
-		void start() override { PDBG(); _stopped = false; }
-		void stop()  override { PDBG(); _stopped = true; }
-
-		Genode::Dataspace_capability dataspace() { PDBG();
-			return _common_state.buffer.cap(); }
 };
 
 
@@ -152,16 +121,43 @@ struct Patch::Main : Genode::Session_request_handler
 {
 	Genode::Env &env;
 
+	struct State_pair
+	{
+		State left;
+		State right;
+
+		State_pair(Genode::Env &env)
+		: left(env), right(env) { }
+	};
+
+	State_pair state { env };
+
+	template <typename SESSION>
+	struct Session_pair
+	{
+		Constructible<SESSION> left  { };
+		Constructible<SESSION> right { };
+
+		Session_capability construct(Genode::Env &env,
+		                             State_pair &state,
+		                             Session_label const &label)
+		{
+			if (label == "left") {
+				left.construct(state.left);
+				return env.ep().manage(*left);
+			}
+			if (label == "right") {
+				right.construct(state.right);
+				return env.ep().manage(*right);
+			}
+			throw Service_denied();
+		}
+	};
+
+	Session_pair< Audio_in::Session_component>  in_sessions { };
+	Session_pair<Audio_out::Session_component> out_sessions { };
+
 	Genode::Session_requests_rom requests_rom { env, *this };
-
-	Patch::State state_left { env };
-	Patch::State state_right { env };
-
-	Constructible<Audio_out::Session_component> source_left { };
-	Constructible<Audio_out::Session_component> source_right { };
-
-	Constructible<Audio_in::Session_component> sink_left { };
-	Constructible<Audio_in::Session_component> sink_right { };
 
 	Main(Genode::Env &env) : env(env)
 	{
@@ -172,58 +168,36 @@ struct Patch::Main : Genode::Session_request_handler
 	                           Parent::Server::Id id,
 	                           Session_state::Args const &args) override
 	{
-		Session_capability cap {};
 		Session_label label_last =
 			label_from_args(args.string()).last_element();
+
 		if (name == "Audio_in") {
-			if (label_last == "left") {
-				sink_left.construct(state_left, env.ep(), id);
-				cap = env.ep().manage(*sink_left);
-			}
-			else
-			if (label_last == "right") {
-				sink_right.construct(state_right, env.ep(), id);
-				cap = env.ep().manage(*sink_right);
-			}
+			env.parent().deliver_session_cap(
+				id, in_sessions.construct(env, state, label_last));
 		} else
 		if (name == "Audio_out") {
-			if (label_last == "left") {
-				source_left.construct(state_left, env.ep(), id);
-				cap = env.ep().manage(*source_left);
+			/* these sessions must be delivered second */
+			if (in_sessions.left.constructed()
+			 && in_sessions.left.constructed())
+			{
+				env.parent().deliver_session_cap(
+					id, out_sessions.construct(env, state, label_last));
 			}
-			else
-			if (label_last == "right") {
-				source_right.construct(state_right, env.ep(), id);
-				cap = env.ep().manage(*source_right);
-			}
-		}
-
-		if (!cap.valid())
+		} else
+		{
 			throw Service_denied();
-		env.parent().deliver_session_cap(id, cap);
+		}
 	}
 
 	void handle_session_upgrade(Parent::Server::Id,
 	                            Session_state::Args const &) override
 	{ }
 
-	void handle_session_close(Parent::Server::Id id) override
+	void handle_session_close(Parent::Server::Id) override
 	{
-		if (source_left.constructed() && source_left->id == id)
-			source_left.destruct();
-		else
-		if (source_right.constructed() && source_right->id == id)
-			source_right.destruct();
-		else
-		if (sink_left.constructed() && sink_left->id == id)
-			sink_left.destruct();
-		else
-		if (sink_right.constructed() && sink_right->id == id)
-			sink_right.destruct();
-
-		env.parent().session_response(id, Parent::SESSION_CLOSED);
+		error("session disconnected");
+		env.parent().exit(0);
 	}
-
 };
 
 
