13c1cbe699eb5b71b922a9170fd360001980df7d
Emery Hemingway <ehmry@posteo.net>
2018-09-17 14:26:41 +0200

    fixup! "Refactor VFS errors" (lxip returns OK 0 on write data error)

--- repos/dde_linux/src/lib/vfs/lxip/vfs.cc
+++ repos/dde_linux/src/lib/vfs/lxip/vfs.cc
@@ -471,7 +471,10 @@ class Vfs::Lxip_data_file : public Vfs::Lxip_file
 
 			Lxip::ssize_t res = _sock.ops->sendmsg(&_sock, &msg, len);
 
-			if (res < 0) _write_err = res;
+			if (res < 0) {
+				_write_err = res;
+				res = 0;
+			}
 
 			return res;
 		}
