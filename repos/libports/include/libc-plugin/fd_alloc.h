4646ab12b415c11e08715d6bd969ced494b3d2a1
Emery Hemingway <ehmry@posteo.net>
2018-10-04 16:46:14 +0200

    Close all libc file-descriptors at exit
    Fix #2996

--- repos/libports/include/libc-plugin/fd_alloc.h
+++ repos/libports/include/libc-plugin/fd_alloc.h
@@ -90,6 +90,11 @@ namespace Libc {
 			void free(File_descriptor *fdo);
 
 			File_descriptor *find_by_libc_fd(int libc_fd);
+
+			/**
+			 * Exposed for emptying the allocator
+			 */
+			using Allocator_avl_tpl<File_descriptor>::apply_any;
 	};
 
 
