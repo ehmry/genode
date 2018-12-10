3d66ea46563ebbc7d5161470ea6b521c1515f9ad
Emery Hemingway <ehmry@posteo.net>
2018-12-10 13:08:08 +0100

    Static ROM session utility
    A simple utility for hosting a ROM session with static content.

---
+++ repos/os/include/os/static_rom_session.h
@@ -0,0 +1,87 @@
+/*
+ * \brief  ROM session implementation for serving static content
+ * \author Emery Hemingway
+ * \date   2018-12-08
+ */
+
+/*
+ * Copyright (C) 2018 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#ifndef _INCLUDE__OS__STATIC_ROM_SESSION_H_
+#define _INCLUDE__OS__STATIC_ROM_SESSION_H_
+
+#include <util/reconstructible.h>
+#include <base/rpc_server.h>
+#include <base/session_label.h>
+#include <base/attached_ram_dataspace.h>
+#include <rom_session/rom_session.h>
+
+namespace Genode { class Static_rom_session; }
+
+
+class Genode::Static_rom_session : public Rpc_object<Rom_session>
+{
+	private:
+
+		Rpc_entrypoint         &_ep;
+		Attached_ram_dataspace  _ds;
+
+	public:
+
+		/**
+		 * Constructor
+		 *
+		 * \param ep                entrypoint serving the ROM session
+		 * \param pd                PD session used to allocate the backing
+		 *                          store for the dataspace handed out to the
+		 *                          client
+		 * \param rm                local region map ('env.rm()') required to
+		 *                          make the dataspace locally visible to
+		 *                          populate its content
+		 *
+		 * \param content           ROM dataspace content
+		 *
+		 * \param size              size of ROM dataspace content
+		 *
+		 * The 'Static_rom_session' associates/disassociates itself with 'ep'.
+		 */
+		Static_rom_session(Rpc_entrypoint &ep,
+		                   Pd_session     &pd,
+		                   Region_map     &rm,
+		                   void const     *content,
+		                   size_t          size)
+		:
+			_ep(ep), _ds(pd, rm, size)
+		{
+			memcpy(_ds.local_addr<char>(), content,
+			       min(size, _ds.size()));
+			_ep.manage(this);
+		}
+
+		~Static_rom_session() { _ep.dissolve(this); }
+
+
+		/***************************
+		 ** ROM session interface **
+		 ***************************/
+
+		Rom_dataspace_capability dataspace() override
+		{
+			Dataspace_capability ds_cap = _ds.cap();
+			return static_cap_cast<Rom_dataspace>(ds_cap);
+		}
+
+		bool update() override { return true; }
+
+		void sigh(Signal_context_capability sigh) override
+		{
+			if (sigh.valid())
+				Signal_transmitter(sigh).submit();
+		}
+};
+
+#endif /* _INCLUDE__OS__STATIC_ROM_SESSION_H_ */
