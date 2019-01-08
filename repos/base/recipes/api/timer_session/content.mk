f8e73c1fe82dbe7e78948bdf5cdd81c213cd1dc1
Christian Helmuth <christian.helmuth@genode-labs.com>
2019-01-08 09:15:25 +0100

    fixup "Move timer from os to base repository"

--- repos/base/recipes/api/timer_session/content.mk
+++ repos/base/recipes/api/timer_session/content.mk
@@ -1,2 +1,9 @@
 MIRRORED_FROM_REP_DIR := include/timer_session include/timer
-include $(REP_DIR)/recipes/api/session.inc
+
+content: $(MIRRORED_FROM_REP_DIR) LICENSE
+
+$(MIRRORED_FROM_REP_DIR):
+	$(mirror_from_rep_dir)
+
+LICENSE:
+	cp $(GENODE_DIR)/LICENSE $@
