b86df74cae9e4f362e298399f18415fcf9336c68
Norman Feske <norman.feske@genode-labs.com>
2019-01-09 14:42:36 +0100

    fixup "Move timer from os to base repository" (test-timed_semaphore)

--- repos/base/recipes/src/test-timed_semaphore/content.mk
+++ repos/base/recipes/src/test-timed_semaphore/content.mk
@@ -1,7 +1,7 @@
-SRC_DIR = src/test/timed_semaphore src/lib/timed_semaphore
+SRC_DIR = src/test/timed_semaphore src/lib/timed_semaphore src/lib/alarm
 include $(GENODE_DIR)/repos/base/recipes/src/content.inc
 
-MIRROR_FROM_REP_DIR := lib/mk/timed_semaphore.mk
+MIRROR_FROM_REP_DIR := lib/mk/timed_semaphore.mk lib/mk/alarm.mk
 
 content: $(MIRROR_FROM_REP_DIR)
 
